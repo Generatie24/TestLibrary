a56de9b424f5e7c11e8a78dfd356ccd2d0174b7c
Kenan Kurda <intecbrussel24@gmail.com>
2024-01-15 15:35:00 +0100

    Add project files.

---
+++ TestLibrary/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibraryValidator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestLibrary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test i Is Integer");
+            string input = Console.ReadLine();
+
+            var result = Validator.IsInteger(input);
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
